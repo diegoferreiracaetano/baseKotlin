4c380d4e6aa7f30635a05be7dedde2add3d92613
Diego <dcaetano@autodoc.com.br>
2018-10-11 00:40:16 -0300

    commit inicial

--- app/src/test/java/com/diegoferreiracaetano/github/ui/RepoViewModelTest.kt
+++ app/src/test/java/com/diegoferreiracaetano/github/ui/RepoViewModelTest.kt
@@ -1,21 +1,15 @@
 package com.diegoferreiracaetano.github.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.paging.DataSource
-import androidx.paging.PagedList
-import com.diegoferreiracaetano.domain.repo.Repo
 import com.diegoferreiracaetano.domain.repo.interactor.CallbackRepoInteractor
 import com.diegoferreiracaetano.domain.repo.interactor.GetListRepoInteractor
-import com.diegoferreiracaetano.github.mock.GitHubFake
+import com.diegoferreiracaetano.github.mock.RepoDataSource
 import com.diegoferreiracaetano.github.ui.repo.RepoViewModel
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 class RepoViewModelTest{
@@ -38,7 +32,8 @@ class RepoViewModelTest{
 
     @Test
     @Throws(Exception::class)
-    fun `Given repos, When load repos, Should update result`() {
+    fun `Given repos, When loadR repos, Should update result`() {
+           val dataSource =  RepoDataSource.RepoDataSourceFactory()
 
     }
 }
